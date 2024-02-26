d1f8a5c4b8f525b7e8513c1eb7404114ef6170f2
Mug1vara97 <mugivarakun97@gmail.com>
2024-02-26 09:35:20 +0300

    1

---
+++ src/loginwindow.h
@@ -0,0 +1,17 @@
+#ifndef LOGINWINDOW_H
+#define LOGINWINDOW_H
+
+#include <QWidget>
+
+class LoginWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    LoginWindow(QWidget *parent = nullptr);
+    ~LoginWindow();
+};
+
+
+
+#endif
